96305e706eb3736e71bb66759bcae64d105c0d27
hema555 <51548468+hema555@users.noreply.github.com>
2019-06-20 09:21:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['502509585370841098'];
 const db = require('quick.db');
 const premium = ['502509585370841098']
 const client = new Discord.Client();   
@@ -310,6 +310,18 @@ client.on('message', function(msg) {
     })
 }
 });
+client.on("message", message => {
+    if (message.content.startsWith("!nobc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
  client.on('message', message => {
               if (!message.channel.guild) return;
       if(message.content =='G.count')
